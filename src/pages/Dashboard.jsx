0bfdb9c069ce988d4f857cd3ce5980d749e18196
aleyna-yildizli <aleynayildizli@gmail.com>
2024-06-14 19:20:03 +0300

    fix: dasboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -38,12 +38,31 @@ const Dashboard = () => {
     return total + paidAmount;
   }, 0);
 
+  const remainingDebt = totalDebt - paidDebt;
+
+  const totalInstallments = debts.reduce((total, debt) => {
+    return total + debt.paymentPlan.length;
+  }, 0);
+
   const upcomingPayments = debts
     .flatMap((debt) => debt.paymentPlan)
     .filter(
       (payment) => !payment.isPaid && new Date(payment.paymentDate) > new Date()
     )
     .sort((a, b) => new Date(a.paymentDate) - new Date(b.paymentDate))
+    .slice(0, 5);
+
+  const paymentHistory = debts
+    .flatMap((debt) =>
+      debt.paymentPlan
+        .filter((payment) => payment.isPaid)
+        .map((payment) => ({
+          debtName: debt.debtName,
+          paymentDate: payment.paymentDate,
+          paymentAmount: payment.paymentAmount,
+        }))
+    )
+    .sort((a, b) => new Date(b.paymentDate) - new Date(a.paymentDate))
     .slice(0, 5);
 
   const formatDate = (dateString) => {
@@ -56,28 +75,42 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="flex-1 w-full h-screen flex flex-col bg-gray-100">
+    <div className="flex-1 w-full h-screen flex flex-col bg-white">
       <div className="flex-1 flex flex-col items-center">
-        <h1 className="text-4xl font-bold my-6">Dashboard</h1>
-        <div className="w-full max-w-4xl flex flex-wrap justify-between gap-4">
-          <div className="w-full md:w-1/3 bg-white p-6 rounded-lg shadow-md">
-            <h2 className="text-xl font-semibold">Toplam Borç</h2>
-            <p className="text-2xl font-bold text-red-600">
+        <div className="w-full max-w-4xl grid grid-cols-2 gap-4 mb-4 my-6">
+          <div className=" p-6 rounded-lg shadow-md bg-sky-800">
+            <h2 className="text-lg font-semibold text-white">Toplam Borç</h2>
+            <p className="text-2xl font-bold text-white ">
               {totalDebt.toFixed(2)} ₺
             </p>
           </div>
-          <div className="w-full md:w-1/3 bg-white p-6 rounded-lg shadow-md">
-            <h2 className="text-xl font-semibold">Ödenen Borç</h2>
-            <p className="text-2xl font-bold text-green-500">
+          <div className="p-6 rounded-lg shadow-md bg-blue-300">
+            <h2 className="text-lg font-semibold text-white">Ödenen Borç</h2>
+            <p className="text-2xl font-bold text-white">
               {paidDebt.toFixed(2)} ₺
             </p>
           </div>
-          <div className="w-full md:w-1/2 bg-white p-2 rounded-lg shadow-md">
-            <h2 className="text-xl font-semibold flex text-center">
+          <div className="p-6 rounded-lg shadow-md bg-violet-400">
+            <h2 className="text-lg font-semibold text-white">Kalan Borç</h2>
+            <p className="text-2xl font-bold text-white">
+              {remainingDebt.toFixed(2)} ₺
+            </p>
+          </div>
+          <div className="p-6 rounded-lg shadow-md bg-emerald-300">
+            <h2 className="text-lg font-semibold text-white">
+              Toplam Taksit Sayısı
+            </h2>
+            <p className="text-2xl font-bold text-white">{totalInstallments}</p>
+          </div>
+        </div>
+
+        <div className="w-full max-w-4xl grid grid-cols-2 gap-4 mb-4">
+          <div className="bg-amber-200 p-6 rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold text-white">
               Yaklaşan Ödeme Tarihleri
             </h2>
             {upcomingPayments.length > 0 ? (
-              <ul className="list-disc ml-4">
+              <ul className="list-disc ml-4 text-white">
                 {upcomingPayments.map((payment, index) => (
                   <li key={index} className="text-lg">
                     {formatDate(payment.paymentDate)}:{" "}
@@ -86,15 +119,33 @@ const Dashboard = () => {
                 ))}
               </ul>
             ) : (
-              <p className="text-lg text-center">
+              <p className="text-lg font-semibold text-white">
                 Yaklaşan ödemeniz bulunmamaktadır.
               </p>
             )}
           </div>
+          <div className="bg-indigo-200 p-6 rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold text-white">Ödeme Geçmişi</h2>
+            {paymentHistory.length > 0 ? (
+              <ul className="list-disc ml-4 text-white">
+                {paymentHistory.map((payment, index) => (
+                  <li key={index} className="text-lg">
+                    {payment.debtName} borcu{" "}
+                    {formatAmount(payment.paymentAmount)} ₺ ödendi
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className="text-lg text-center text-white">
+                Ödeme geçmişiniz bulunmamaktadır.
+              </p>
+            )}
+          </div>
         </div>
-        <div className="w-full max-w-4xl mt-8 bg-white p-6 rounded-lg shadow-md">
+
+        <div className="w-full max-w-4xl mt-8 bg-white p-6 rounded-lg shadow-md mb-10">
           <h2 className="text-xl font-semibold mb-4">Borç Durumu Grafiği</h2>
-          <DebtChart />
+          <DebtChart debts={debts} />
         </div>
       </div>
     </div>
